a042241d2672aef7103f255b7ea0bf95f21c47a9
KanterAlon <163696462+KanterAlon@users.noreply.github.com>
2024-05-15 17:11:13 -0300

    Update Cliente.cs

--- Cliente.cs
+++ Cliente.cs
@@ -1,8 +1,10 @@
-﻿public class Cliente
+using System;
+
+public class Cliente
 {
-    public int DNI { get;  set; }
-    public string Nombre { get;  set; }
-    public string Apellido { get;  set; }
+    public int DNI { get; set; }
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
     public DateTime FechaInscripcion { get; set; }
     public int TipoEntrada { get; set; }
     public int Cantidad { get; set; }
@@ -10,6 +12,8 @@
 
     public Cliente(int id, string nombre, string apellido, int dni, DateTime fechaInscripcion, int tipoEntrada, int cantidad, int abono)
     {
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.ForegroundColor = ConsoleColor.White;
         Nombre = nombre;
         Apellido = apellido;
         DNI = dni;
@@ -17,10 +21,14 @@
         TipoEntrada = tipoEntrada;
         Cantidad = cantidad;
         Abono = abono;
-        
+        Console.ResetColor();
     }
 
     public Cliente()
     {
+        Console.BackgroundColor = ConsoleColor.Green;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.WriteLine("Nuevo cliente creado");
+        Console.ResetColor();
     }
 }
